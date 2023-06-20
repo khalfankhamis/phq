3b69126531787d7857dd9b48ff0ab5dcb7457179
unknown <khalfankhamis81@gmail.com>
2023-06-20 08:50:18 +0300

    my first startup

---
+++ src/main/java/com/hris/phq/PhqApplication.java
@@ -0,0 +1,13 @@
+package com.hris.phq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hris/phq/PhqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hris.phq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
